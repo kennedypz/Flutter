37feb290aad7f7320133968add38309e50410eca
Paulo Kennedy <paulo_lavigne@hotmail.com>
2020-01-06 13:35:18 -0300

    ***APP*** A simple chat

---
+++ chat_app/lib/chat_screen.dart
@@ -0,0 +1,77 @@
+import 'package:chat_app/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget{
+  @override 
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen>{
+  final TextEditingController _textController = TextEditingController();
+
+  List<ChatMessage> _messages = [];
+
+  void _handleSubmitted(String text){
+    _textController.clear();
+    ChatMessage message = ChatMessage(text: text);
+
+    setState(() {
+     _messages.insert(0, message); 
+    });
+  }
+
+
+  Widget _textComposerWidget(){
+    return IconTheme(
+      data: IconThemeData(
+        color: Colors.lightBlue,
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration.collapsed(
+                hintText: 'Send a message...',
+                ),
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override 
+  Widget build(BuildContext context){
+    return Column(
+      children: <Widget>[
+        Flexible(
+          child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          ),
+        ),
+        Divider(height: 1.0),
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).cardColor,
+          ),
+          child: _textComposerWidget(),
+        ),
+      ],
+    );
+  }
+}
